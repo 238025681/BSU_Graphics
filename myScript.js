c26685279257f07c9eff0466f6cb4b30267d2537
238025681 <kalin.chobanov@gmail.com>
2018-03-27 19:11:36 +0300

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,17 +1,24 @@
 "use strict";
 
-	function getCordinates(){
-		alert("click");
-	}
+function getCordinates() {
+    alert("click");
+}
 
 
 
 
 var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
+var ctx = c.getContext("2d");
 
-	drawSH();
+//================= ������ =======================================
 
+let base_line = {startX: 100, startY: 200, endX: 500, endY: 200};
+let color = "black";
+let template = [6, 3, 1, 3];
+
+drawSH(base_line, template, color);
+
+//================= ������ =======================================
 
 let line = {
     startX: 100,
@@ -20,7 +27,7 @@ let line = {
     endY: 200
 };
 
-let color = "black";
+color = "black";
 //drawLine(line, color);
 
 
@@ -84,10 +91,10 @@ let newPoint = (m * l2.startX) + b;
 //color = "green";
 //let point = {X: 100, Y: 100}
 //let line_5 = {
- //   startX: 100,
- //   startY: 100,
- //   endX: 200,
- //   endY: 200
+//   startX: 100,
+//   startY: 100,
+//   endX: 200,
+//   endY: 200
 //};
 
 //drawLine(rotationByPoint(line_5, point, 10), color);
@@ -262,149 +269,150 @@ function symmetryByOxOy(line) {
 
 //================================================//
 //=========Ротация на ъгъл  спрямо точка C, с координати (Xc,Yc), различна от началото на координатната система:=======================================//
- 
- 
+
+
 function rotationByPoint(line, point, angle) {
 
     let rotationByPointLine = line;
 
     rotationByPointLine = translationWhitVector(line, -point.X, -point.Y);
-   
+
     rotationByPointLine = rotationWhitAngle(rotationByPointLine, angle);
 
     rotationByPointLine = translationWhitVector(rotationByPointLine, point.X, point.Y);
- 
-    
+
+
 
     return rotationByPointLine;
 
 
 }
- 
-  
+
+
 //================================================//
 
 
-let rectangle = { xMin: 100, xMax: 200, yMin: 100, yMax: 200 }
+let rectangle = {xMin: 100, xMax: 200, yMin: 100, yMax: 200}
 
 ctx.rect(rectangle.xMin, rectangle.yMin, 100, 100);
 //ctx.stroke();
-let temp_line = { startX: 90 , startY: 90, endX: 190, endY: 190 }
+let temp_line = {startX: 90, startY: 90, endX: 190, endY: 190}
 //drawLine(temp_line, "red");
 
 
-let startPoint = { x: temp_line.startX, y: temp_line.startY };
-let endPoint = { x: temp_line.endX, y: temp_line.endY };
+let startPoint = {x: temp_line.startX, y: temp_line.startY};
+let endPoint = {x: temp_line.endX, y: temp_line.endY};
 
 
 
 const inside = 0;
-const left = 1 ;
-const right = 2 ;
+const left = 1;
+const right = 2;
 const bottom = 4;
 const TOP = 8;
 
-cohenSutherlandLineClipAndDraw(rectangle, startPoint, endPoint );
+cohenSutherlandLineClipAndDraw(rectangle, startPoint, endPoint);
 
-function computOutCode(rectangle, point){
-   
-   let code = inside  ;
-   if(point.x < rectangle.xMin) {
-     code |= left;
- 
- }else if(point.x > rectangle.xMax){
-  
-  code |= right;
+function computOutCode(rectangle, point) {
 
-}
+    let code = inside;
+    if (point.x < rectangle.xMin) {
+        code |= left;
 
-if(point.y < rectangle.yMin ){
+    } else if (point.x > rectangle.xMax) {
 
-  code |= bottom;
+        code |= right;
 
-}else if(point.y > rectangle.yMax ){
-  
-  code |= TOP;
-
-}
-
-return code;
-
-};
-
-
-function cohenSutherlandLineClipAndDraw(rectangle, startPoint, endPoint ){
-
-   let startPointCode = computOutCode(rectangle, startPoint);
-   let endPointCode = computOutCode(rectangle, endPoint);
-  
-   let accept = false;
-  // console.log(startPointCode);
-//   console.log(endPointCode);
-  
-   while(true){
-    if(! (startPointCode | endPointCode )){
-   
-      accept = true ;
-      break;
-    }else if ( startPointCode & endPointCode ){
-      break;
-    }else {
-   
-      let x, y;
-      let outcodeOut = startPointCode ? startPointCode : endPointCode;
- 
- 
- //console.log(endPointCode);
-
-      if (outcodeOut & TOP) {           // point is above the clip window
-         x = startPoint.x + (endPoint.x - startPoint.x) * (rectangle.yMax - startPoint.y) / (endPoint.y - startPoint.y);
-         y = rectangle.yMax;
-      }else if (outcodeOut & bottom) { // point is below the clip window
-         x = startPoint.x  + (endPoint.x  - startPoint.x ) * (rectangle.yMin - startPoint.y) / (endPoint.y  - startPoint.y );
-         y = rectangle.yMin;
-      }else if (outcodeOut & right ) {  // point is to the right of clip window
-         y = startPoint.y  + (endPoint.y  - startPoint.y ) * (rectangle.xMax  - startPoint.x ) / (endPoint.x - startPoint.x );
-         x = rectangle.xMax ;
-      }else if (outcodeOut & left ) {   // point is to the left of clip window
-         y = startPoint.y  + (endPoint.y  - startPoint.y ) * (rectangle.xMin  - startPoint.x ) / (endPoint.x  - startPoint.x );
-         x = rectangle.xMin ;
-      }
-     
-    
-      if (outcodeOut === startPointCode ) {
-         startPoint.x  = x;
-         startPoint.y  = y;
-         startPointCode  = computOutCode(rectangle, startPoint);
-      } else {
-      endPoint.x = x;
-      endPoint.y  = y;
-      endPointCode  = computOutCode(rectangle , endPoint );
-      }
     }
-    
- 
- 
-   }
-  
-  if (accept) {
-  // Following functions are left for implementation by user based on
-  // their platform (OpenGL/graphics.h etc.)
-  
-  temp_line = { startX: startPoint.x , startY: startPoint.y , endX: endPoint.x, endY: endPoint.y }
-  //console.log (JSON.stringify(temp_line) );
-  
-  
-  
- // drawLine(temp_line, "red");
-  
-  }
-   
+
+    if (point.y < rectangle.yMin) {
+
+        code |= bottom;
+
+    } else if (point.y > rectangle.yMax) {
+
+        code |= TOP;
+
+    }
+
+    return code;
+
+}
+;
+
+
+function cohenSutherlandLineClipAndDraw(rectangle, startPoint, endPoint) {
+
+    let startPointCode = computOutCode(rectangle, startPoint);
+    let endPointCode = computOutCode(rectangle, endPoint);
+
+    let accept = false;
+    // console.log(startPointCode);
+//   console.log(endPointCode);
+
+    while (true) {
+        if (!(startPointCode | endPointCode)) {
+
+            accept = true;
+            break;
+        } else if (startPointCode & endPointCode) {
+            break;
+        } else {
+
+            let x, y;
+            let outcodeOut = startPointCode ? startPointCode : endPointCode;
+
+
+            //console.log(endPointCode);
+
+            if (outcodeOut & TOP) {           // point is above the clip window
+                x = startPoint.x + (endPoint.x - startPoint.x) * (rectangle.yMax - startPoint.y) / (endPoint.y - startPoint.y);
+                y = rectangle.yMax;
+            } else if (outcodeOut & bottom) { // point is below the clip window
+                x = startPoint.x + (endPoint.x - startPoint.x) * (rectangle.yMin - startPoint.y) / (endPoint.y - startPoint.y);
+                y = rectangle.yMin;
+            } else if (outcodeOut & right) {  // point is to the right of clip window
+                y = startPoint.y + (endPoint.y - startPoint.y) * (rectangle.xMax - startPoint.x) / (endPoint.x - startPoint.x);
+                x = rectangle.xMax;
+            } else if (outcodeOut & left) {   // point is to the left of clip window
+                y = startPoint.y + (endPoint.y - startPoint.y) * (rectangle.xMin - startPoint.x) / (endPoint.x - startPoint.x);
+                x = rectangle.xMin;
+            }
+
+
+            if (outcodeOut === startPointCode) {
+                startPoint.x = x;
+                startPoint.y = y;
+                startPointCode = computOutCode(rectangle, startPoint);
+            } else {
+                endPoint.x = x;
+                endPoint.y = y;
+                endPointCode = computOutCode(rectangle, endPoint);
+            }
+        }
+
+
+
+    }
+
+    if (accept) {
+        // Following functions are left for implementation by user based on
+        // their platform (OpenGL/graphics.h etc.)
+
+        temp_line = {startX: startPoint.x, startY: startPoint.y, endX: endPoint.x, endY: endPoint.y}
+        //console.log (JSON.stringify(temp_line) );
+
+
+
+        // drawLine(temp_line, "red");
+
+    }
+
 
 }
 
 
-    
+
 
 
 
@@ -416,71 +424,60 @@ function cohenSutherlandLineClipAndDraw(rectangle, startPoint, endPoint ){
 
 
 
-function drawSH() {
+function drawSH(base_line, template, color) {
 
 
-    let s_line = { startX: 50, startY: 100, endX: 100, endY: 100 };
-  
-    let array = [4, 2, 2, 2];
+    let count = (base_line.endX - base_line.startX) > (base_line.endY - base_line.startY) ?
+            Math.abs((base_line.endX - base_line.startX) / template.length) :
+            Math.abs((base_line.endY - base_line.startY) / template.length);
 
-    let k1 = 0;
-    let l_length = Math.pow(Math.pow(s_line.endX - s_line.startX,2) + Math.pow(s_line.endY - s_line.startY,0.5),0.5);
-    
-    let k2 = array[1] / l_length;
-    let count = Math.round(l_length / array.length);
-        let x0 = s_line.startX;
-        let y0 = s_line.startY;
-        let x1 = Math.round(((1 - k2) * s_line.startX) + (k2 * s_line.endX)); //x0+4
-        let y1 = Math.round(((1 - k2) * s_line.startY) + (k2 * s_line.endY));
 
-  
+    let scale = Math.pow(Math.pow(base_line.endX - base_line.startX, 2) + Math.pow(base_line.endY - base_line.startY, 0.5), 0.5);
+    let startRatio = 0;
+    let endRatio = template[1] / scale;
+    let templateLine = {
+        startX: base_line.startX,
+        startY: base_line.startY,
+        endX: Math.round(((1 - endRatio) * base_line.startX) + (endRatio * base_line.endX)),
+        endY: Math.round(((1 - endRatio) * base_line.startY) + (endRatio * base_line.endY))
+    };
 
-    for (let i = 2  ; i <= count; i++) {
+    let i;
 
-        //	o	Ако k1  > 1, полагаме k1  = 1  /Q1  ?  P2/ и излизаме от цикъла.
-        //o	Ако k2  > 1, полагаме k2  = 1  /Q2  ?  P1/ и излизаме от цикъла.
+    for (i = 2; i < count; i++) {
 
-        let color = "pink";
+        drawLine(templateLine, color);
 
-        
-        
-        drawLine({startX: x0, startY: y0, endX: x1, endY: y1}, color)
+        startRatio = endRatio + template[(2 * i - 1) % template.length] / scale;
+        endRatio = startRatio + template[(2 * i - 2) % template.length] / scale;
 
-       
-       
-        k1 = (k2 + array[(2 * i - 2) % array.length]) / l_length;
-        k2 = (k1 + array[(2 * i - 1) % array.length]) / l_length;
-        
-        
-        
+        templateLine.startX = Math.round(((1 - startRatio) * base_line.startX) + (startRatio * base_line.endX));
+        templateLine.startY = Math.round(((1 - startRatio) * base_line.startY) + (startRatio * base_line.endY));
+        templateLine.endX = Math.round(((1 - endRatio) * base_line.startX) + (endRatio * base_line.endX));
+        templateLine.endY = Math.round(((1 - endRatio) * base_line.startY) + (endRatio * base_line.endY));
 
-       
-        
-        x0 += Math.round(((k1) * s_line.endX));
-        y0 = Math.round(((1 - k1) * s_line.startY) + (k1 * s_line.endY));
-        x1 += Math.round( (k2 * s_line.endX)); 
-        y1 = Math.round(((1 - k2) * s_line.startY) + (k2 * s_line.endY));
-        
 
-       
-        console.log("X0 Start01 - " + x0);
-        
-        console.log("End x01 - " +  x1);
-        
-       
-       
 
-        if (k1 > 1) {
-            k1 = 1;
+        if (startRatio > 1) {
+            startRatio = 1;
             break;
         }
-        if (k2 > 1) {
-            k2 = 1;
+
+        if (endRatio > 1) {
+
+            endRatio = 1;
             break;
         }
 
     }
-    
+
+
+    templateLine.startX = Math.round(((1 - startRatio) * base_line.startX) + (startRatio * base_line.endX));
+    templateLine.startY = Math.round(((1 - startRatio) * base_line.startY) + (startRatio * base_line.endY));
+    templateLine.endX = base_line.endX;
+    templateLine.endY = base_line.endY;
+
+    drawLine(templateLine, color);
 
 }
 
